010085d64a52ec2cfc51a10e443b5251e057a3ee
victor.de.s.alves <victorsouzz@gmail.com>
2024-01-19 13:22:40 -0300

    melhoria no metodo de buscas

--- lib/utils/book_search.dart
+++ lib/utils/book_search.dart
@@ -1,3 +1,4 @@
+import 'dart:convert' show ascii;
 import 'package:wolf_library/model/book.dart';
 
 class BookSearch {
@@ -18,13 +19,27 @@ class BookSearch {
   void _indexBook(String text, Book book) {
     var words = text.split(' ');
     for (var word in words) {
-      var key = word.toLowerCase();
+      var key = _normalize(word);
       _index.putIfAbsent(key, () => <Book>{}).add(book);
     }
   }
 
+  String _normalize(String word) {
+    return ascii.decode(
+        word.toLowerCase().runes.map((r) => r < 128 ? r : _fallback(r)).toList());
+  }
+
+  int _fallback(int rune) {
+    const accents = 'áàâäãåçéèêëíìîïñóòôöõúùûüýÿ';
+    const nonAccents = 'aaaaaaceeeeiiiinooooouuuuyy';
+
+    var index = accents.runes.toList().indexOf(rune);
+    return index != -1 ? nonAccents.codeUnitAt(index) : rune;
+  }
+
   List<Book> search(String query) {
-    var words = query.toLowerCase().split(' ');
+    var normalizedQuery = _normalize(query);
+    var words = normalizedQuery.split(' ');
     Set<Book>? resultBooks;
     for (var word in words) {
       var booksForKey = _index[word];
@@ -33,9 +48,25 @@ class BookSearch {
             ? resultBooks.intersection(booksForKey)
             : booksForKey;
       } else {
-        return []; // Se uma palavra da consulta não estiver no índice, não há correspondências
+        // Busca por letras iniciais, se necessário
+        for (int i = 1; i <= word.length && resultBooks == null; i++) {
+          resultBooks = _searchByInitialLetters(word.substring(0, i));
+        }
+        if (resultBooks == null) return [];
       }
     }
     return resultBooks?.toList() ?? [];
+  }
+
+  Set<Book>? _searchByInitialLetters(String letters) {
+    Set<Book>? booksStartingWith;
+    for (var key in _index.keys) {
+      if (key.startsWith(letters)) {
+        booksStartingWith = booksStartingWith != null
+            ? booksStartingWith.union(_index[key]!)
+            : _index[key];
+      }
+    }
+    return booksStartingWith;
   }
 }
